b39b8e3db12cd8ac331927c68d73f2c5d3947422
Troy <tanaka_s2@yahoo.com>
2024-12-12 14:05:54 -0600

    1.2.1

--- scripts/Game/VON/Modded/SCR_VONController.c
+++ scripts/Game/VON/Modded/SCR_VONController.c
@@ -6,30 +6,19 @@ modded class SCR_VONController
 	[Attribute(defvalue: "Sounds/VON/beepend.wav", params: "wav")]
 	protected ResourceName m_sRadioSoundEnd;
 	
-	protected AudioHandle m_AudioHandleRadioSqawk;
-	protected AudioHandle m_AudioHandleRadioSqawkRadioSqawkOff;
-	
- 	override protected void OnVONChannel(float value, EActionTrigger reason)
+	override void ActivateVON(EVONTransmitType transmitType)
 	{
-		if (reason == EActionTrigger.DOWN)
-		{
-			m_bIsActiveModeLong = false;
-			if (!AudioSystem.IsSoundPlayed(m_AudioHandleRadioSqawk))
-					AudioSystem.TerminateSound(m_AudioHandleRadioSqawk);
-				
-			m_AudioHandleRadioSqawk = AudioSystem.PlaySound(m_sRadioSound);			
-			
-			VONChannel(true);
-		}
-		else {
-				AudioSystem.TerminateSound(m_AudioHandleRadioSqawk);
-				
-				if (!AudioSystem.IsSoundPlayed(m_AudioHandleRadioSqawkRadioSqawkOff))
-						AudioSystem.TerminateSound(m_AudioHandleRadioSqawkRadioSqawkOff);
-				
-				m_AudioHandleRadioSqawkRadioSqawkOff = AudioSystem.PlaySound(m_sRadioSoundEnd);
-			
-				VONChannel(false);
-		}
+		super.ActivateVON(transmitType);
+		
+		if (transmitType != EVONTransmitType.DIRECT)
+			AudioSystem.PlaySound(m_sRadioSound);
+	}
+	
+	override void DeactivateVON(EVONTransmitType transmitType = EVONTransmitType.NONE)
+	{
+		super.DeactivateVON(transmitType);
+		
+		if (transmitType != EVONTransmitType.DIRECT)
+			AudioSystem.PlaySound(m_sRadioSoundEnd);	
 	}
 };
